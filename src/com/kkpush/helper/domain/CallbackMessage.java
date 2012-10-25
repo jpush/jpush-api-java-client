3dcfe93b0a728ed3973fef69cc5ad6a081e25098
x1a0d@n9 <caosd@oa.kktalk.cn>
2012-10-25 09:47:08 +0800

    upload test

---
+++ src/com/kkpush/helper/domain/CallbackMessage.java
@@ -0,0 +1,52 @@
+package com.kkpush.helper.domain;
+
+/*
+ * 发送消息回调的数据格式
+ * 与消息发送接口的 callback_url 参数相对应，极光Push Server消息发送结束时，调用此回调接口，以向调用者反馈发送结果。
+ * 请开发者提供此接口支持 HTTP POST 请求。
+ * 请求参数名称是："push_result"。
+ */
+public class CallbackMessage {
+	//发送序号
+	private int sendno;
+	//错误码，详见com.kkpush.helper.enums.message.ErrorCodeEnum
+	private int errcode;
+	//错误说明
+	private String errmsg;
+	//本次推送满足条件的用户数
+	private int total_user;
+	//本次推送成功发送的用户数
+	private int send_cnt;
+	
+	public int getSendno() {
+		return sendno;
+	}
+	public void setSendno(int sendno) {
+		this.sendno = sendno;
+	}
+	public int getErrcode() {
+		return errcode;
+	}
+	public void setErrcode(int errcode) {
+		this.errcode = errcode;
+	}
+	public String getErrmsg() {
+		return errmsg;
+	}
+	public void setErrmsg(String errmsg) {
+		this.errmsg = errmsg;
+	}
+	public int getTotal_user() {
+		return total_user;
+	}
+	public void setTotal_user(int total_user) {
+		this.total_user = total_user;
+	}
+	public int getSend_cnt() {
+		return send_cnt;
+	}
+	public void setSend_cnt(int send_cnt) {
+		this.send_cnt = send_cnt;
+	}
+	
+}
---
+++ src/com/kkpush/helper/domain/NotifyMessageParam.java
@@ -0,0 +1,63 @@
+package com.kkpush.helper.domain;
+
+/*
+ * 通知类型的消息内容
+ */
+public class NotifyMessageParam extends MessageParam {
+	public NotifyMessageParam() {
+		
+	}
+	
+	public NotifyMessageParam(String username, String password) {
+		super(username, password);
+	}
+	
+	public class NotifyMsgContent extends MessageParam.MsgContent {
+		//不填则默认为 0，使用 极光Push SDK 的默认通知样式。
+		private String builderId = "";
+		
+		//通知标题。不填则默认使用该应用的名称。
+		private String title = "";
+		
+		//通知内容。
+		private String content = "";
+	
+		public String getBuilderId() {
+			return builderId;
+		}
+		public void setBuilderId(String builderId) {
+			this.builderId = builderId;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("{");
+			buffer.append("\"n_content\":\"" + this.getContent() + "\"");
+			if (null != this.getBuilderId()) {
+				buffer.append(",\"n_builder_id\":\"" + this.getBuilderId() + "\"");
+			}
+			if (null != this.getTitle()) {
+				buffer.append(",\"n_title\":\"" + this.getTitle() + "\"");
+			}
+			buffer.append("}");
+			return buffer.toString();
+		}
+	}
+	private NotifyMsgContent msgContent = new NotifyMsgContent();
+	public NotifyMsgContent getMsgContent() {
+		return this.msgContent;
+	}
+}
