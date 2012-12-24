5cb823907828ed0c5917b3adfd0df84490f084cf
Javen <fajaven@gmail.com>
2012-12-24 15:37:43 +0800

    Add junit test.

---
+++ test/cn/jpush/api/CharacterTests.java
@@ -0,0 +1,15 @@
+package cn.jpush.api;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CharacterTests {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+    
+}
