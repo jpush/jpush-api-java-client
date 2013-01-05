0ad96e3428f0085b146c2c00e413d27726555730
zengzhiwu <zengzw@oa.kktalk.cn>
2013-01-05 19:01:58 +0800

    update

--- src/cn/jpush/api/BaseURL.java
+++ src/cn/jpush/api/BaseURL.java
@@ -3,10 +3,10 @@ package cn.jpush.api;
 public class BaseURL {
 	
 	private static final String HOST_NAME_SSL = "https://api.jpush.cn:443";
-	private static final String HOST_NAME = "http://117.135.160.250:8800";
-	protected static final String ALL_PATH = "/test/v2/views.py";       //全功能，发送地址
-	protected static final String SIMPLE_CUSTOM_PATH = "/test/v2/custom_message.py"; //简易接口，自定义消息
-	protected static final String SIMPLE_NOTIFICATION_PATH = "/test/v2/notification.py"; //简易接口，自定义通知
+	private static final String HOST_NAME = "http://api.jpush.cn:8800";
+	protected static final String ALL_PATH = "/sendmsg/v2/sendmsg";       //全功能，发送地址
+	protected static final String SIMPLE_CUSTOM_PATH = "/sendmsg/v2/custom_message"; //简易接口，自定义消息
+	protected static final String SIMPLE_NOTIFICATION_PATH = "/sendmsg/v2/notification"; //简易接口，自定义通知
 	
 	
 	private static String getHostname(boolean enableSSL) {
