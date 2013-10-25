88b23e22aaaddc4fecdd966c14af83b0e8faa9cf
zengzhiwu <zengzhiwu@jpush.cn>
2013-10-25 13:27:01 +0800

    update push api url

--- src/cn/jpush/api/BaseURL.java
+++ src/cn/jpush/api/BaseURL.java
@@ -5,9 +5,9 @@ public class BaseURL {
 	public static String HOST_NAME_SSL = "https://api.jpush.cn:443";
 	public static String HOST_NAME = "http://api.jpush.cn:8800";
 	
-	protected static final String ALL_PATH = "/sendmsg/v2/sendmsg";       //全功能，发送地址
-	protected static final String SIMPLE_CUSTOM_PATH = "/sendmsg/v2/custom_message"; //简易接口，自定义消息
-	protected static final String SIMPLE_NOTIFICATION_PATH = "/sendmsg/v2/notification"; //简易接口，自定义通知
+	protected static final String ALL_PATH = "/v2/push";       //全功能接口
+	protected static final String SIMPLE_CUSTOM_PATH = "/v2/custom_message"; //简易接口，自定义消息
+	protected static final String SIMPLE_NOTIFICATION_PATH = "/v2/notification"; //简易接口，自定义通知
 	
 	private static String getHostname(boolean enableSSL) {
 		return enableSSL? HOST_NAME_SSL :HOST_NAME;
