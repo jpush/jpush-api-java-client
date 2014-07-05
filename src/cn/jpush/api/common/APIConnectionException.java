d944a4b7683708c04ce33de2aaa0efd587e4267b
Javen <javen@jpush.cn>
2014-07-05 11:46:11 +0800

    Add tests for connect timed out and read timed out

--- src/cn/jpush/api/common/APIConnectionException.java
+++ src/cn/jpush/api/common/APIConnectionException.java
@@ -12,9 +12,15 @@ package cn.jpush.api.common;
 public class APIConnectionException extends Exception {
     private static final long serialVersionUID = -2615370590441195647L;
     private boolean readTimedout = false;
+    private int doneRetriedTimes = 0;
 
     public APIConnectionException(String message, Throwable e) {
         super(message, e);
+    }
+
+    public APIConnectionException(String message, Throwable e, int doneRetriedTimes) {
+        super(message, e);
+        this.doneRetriedTimes = doneRetriedTimes;
     }
 
     public APIConnectionException(String message, Throwable e, boolean readTimedout) {
@@ -26,6 +32,9 @@ public class APIConnectionException extends Exception {
         return readTimedout;
     }
     
+    public int getDoneRetriedTimes() {
+        return this.doneRetriedTimes;
+    }
 }
 
 
