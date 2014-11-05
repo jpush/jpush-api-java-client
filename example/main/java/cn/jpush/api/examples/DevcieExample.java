e6595c7de4f3229cad9759e8ef638ae671b757dc
Javen <javen@jpush.cn>
2014-11-05 14:43:00 +0800

    1. Upgrade to slf4j-api-1.7.7 for libs/ 2. Add log4j support jars to libs, and project build path.  They are for example running. 3. Add a device example.

--- example/main/java/cn/jpush/api/examples/DevcieExample.java
+++ example/main/java/cn/jpush/api/examples/DevcieExample.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cn.jpush.api.JPushClient;
+import cn.jpush.api.common.resp.APIConnectionException;
+import cn.jpush.api.common.resp.APIRequestException;
+import cn.jpush.api.device.TagAliasResult;
 
 public class DevcieExample {
 	protected static final Logger LOG = LoggerFactory.getLogger(DevcieExample.class);
@@ -16,9 +19,30 @@ public class DevcieExample {
 	private static final String REGISTRATION_ID1 = "0900e8d85ef";
 	private static final String REGISTRATION_ID2 = "0a04ad7d8b4";
 
-	private static JPushClient jpushClient = new JPushClient(masterSecret, appKey, 3);
-	
+	private static JPushClient jpushClient = new JPushClient(masterSecret, appKey);
+
 	public static void main(String[] args) {
-		
+		testGetDeviceTagAlias();
 	}
+	
+	public static void testGetDeviceTagAlias() {
+		try {
+			TagAliasResult result = jpushClient.getDeviceTagAlias(REGISTRATION_ID1);
+			
+			LOG.info(result.alias);
+			LOG.info(result.tags.toString());
+			
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+			
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+            LOG.info("HTTP Status: " + e.getStatus());
+            LOG.info("Error Code: " + e.getErrorCode());
+            LOG.info("Error Message: " + e.getErrorMessage());
+		}
+	}
+	
 }
+
+
