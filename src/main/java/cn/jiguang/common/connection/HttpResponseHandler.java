d20e591904a034571bd330de62dec1ed160eee22
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2017-01-05 17:50:32 +0800

    prepare release v1.0.2

--- src/main/java/cn/jiguang/common/connection/HttpResponseHandler.java
+++ src/main/java/cn/jiguang/common/connection/HttpResponseHandler.java
@@ -1,6 +1,8 @@
 package cn.jiguang.common.connection;
 
+import cn.jiguang.common.resp.APIRequestException;
 import cn.jiguang.common.resp.ResponseWrapper;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
@@ -11,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.CountDownLatch;
 
 
+@ChannelHandler.Sharable
 public class HttpResponseHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     private static final Logger LOG = LoggerFactory.getLogger(HttpResponseHandler.class);
@@ -41,14 +44,55 @@ public class HttpResponseHandler extends SimpleChannelInboundHandler<HttpObject>
                 String responseContent = content.content().toString(CharsetUtil.UTF_8);
                 _wrapper.responseCode = status;
                 _wrapper.responseContent = responseContent;
-                LOG.info("Got Response code: " + status + " content: " + responseContent);
-                System.err.println("Got Response code: " + status + " content: " + responseContent);
-                System.err.flush();
-                if (null != _callback) {
-                    _callback.onSucceed(_wrapper);
+                if (status >= 200 && status < 300) {
+                    LOG.debug("Succeed to get response OK - responseCode:" + status);
+                    LOG.debug("Response Content - " + responseContent);
+
+                } else if (status >= 300 && status < 400) {
+                    LOG.warn("Normal response but unexpected - responseCode:" + status + ", responseContent:" + responseContent);
+
+                } else {
+                    LOG.warn("Got error response - responseCode:" + status + ", responseContent:" + responseContent);
+
+                    switch (status) {
+                        case 400:
+                            LOG.error("Your request params is invalid. Please check them according to error message.");
+                            _wrapper.setErrorObject();
+                            break;
+                        case 401:
+                            LOG.error("Authentication failed! Please check authentication params according to docs.");
+                            _wrapper.setErrorObject();
+                            break;
+                        case 403:
+                            LOG.error("Request is forbidden! Maybe your appkey is listed in blacklist or your params is invalid.");
+                            _wrapper.setErrorObject();
+                            break;
+                        case 404:
+                            LOG.error("Request page is not found! Maybe your params is invalid.");
+                            _wrapper.setErrorObject();
+                            break;
+                        case 410:
+                            LOG.error("Request resource is no longer in service. Please according to notice on official website.");
+                            _wrapper.setErrorObject();
+                        case 429:
+                            LOG.error("Too many requests! Please review your appkey's request quota.");
+                            _wrapper.setErrorObject();
+                            break;
+                        case 500:
+                        case 502:
+                        case 503:
+                        case 504:
+                            LOG.error("Seems encountered server error. Maybe JPush is in maintenance? Please retry later.");
+                            break;
+                        default:
+                            LOG.error("Unexpected response.");
+                    }
                 }
                 if (null != _latch) {
                     _latch.countDown();
+                }
+                if (null != _callback) {
+                    _callback.onSucceed(_wrapper);
                 }
             }
         }
