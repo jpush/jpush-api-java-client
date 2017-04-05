bdf97ed5ed79b7d67d2ad967f78e85871f84a405
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2017-04-05 10:59:29 +0800

    android notification add alert_type

--- src/main/java/cn/jpush/api/push/model/notification/AndroidNotification.java
+++ src/main/java/cn/jpush/api/push/model/notification/AndroidNotification.java
@@ -13,6 +13,7 @@ public class AndroidNotification extends PlatformNotification {
     private static final String BUILDER_ID = "builder_id";
     private static final String INBOX = "inbox";
     private static final String STYLE = "style";
+    private static final String ALERT_TYPE = "alert_type";
     private static final String BIG_TEXT = "big_text";
     private static final String BIG_PIC_PATH = "big_pic_path";
     private static final String PRIORITY = "priority";
@@ -20,15 +21,18 @@ public class AndroidNotification extends PlatformNotification {
     
     private final String title;
     private final int builderId;
+    // 0 ~ 4
     private int style = 0;
+    // -1 ~ 7
+    private int alert_type = -1;
     private String big_text;
     private Object inbox;
     private String big_pic_path;
     private int priority;
     private String category;
     
-    private AndroidNotification(Object alert, String title, int builderId, int style, String bigText, Object inbox,
-                                String bigPicPath, int priority, String category,
+    private AndroidNotification(Object alert, String title, int builderId, int style, int alertType, String bigText,
+                                Object inbox, String bigPicPath, int priority, String category,
             Map<String, String> extras, 
             Map<String, Number> numberExtras, 
             Map<String, Boolean> booleanExtras, 
@@ -38,6 +42,7 @@ public class AndroidNotification extends PlatformNotification {
         this.title = title;
         this.builderId = builderId;
         this.style = style;
+        this.alert_type = alertType;
         this.big_text = bigText;
         this.inbox = inbox;
         this.big_pic_path = bigPicPath;
@@ -83,6 +88,10 @@ public class AndroidNotification extends PlatformNotification {
             json.add(STYLE, new JsonPrimitive(this.style));
         }
 
+        if (-1 != alert_type) {
+            json.add(ALERT_TYPE, new JsonPrimitive(this.alert_type));
+        }
+
         if (null != big_text) {
             json.add(BIG_TEXT, new JsonPrimitive(this.big_text));
         }
@@ -114,6 +123,7 @@ public class AndroidNotification extends PlatformNotification {
         private String title;
         private int builderId;
         private int style = 0;
+        private int alert_type;
         private String big_text;
         private Object inbox;
         private String big_pic_path;
@@ -141,6 +151,11 @@ public class AndroidNotification extends PlatformNotification {
 
         public Builder setStyle(int style) {
             this.style = style;
+            return this;
+        }
+
+        public Builder setAlertType(int alertType) {
+            this.alert_type = alertType;
             return this;
         }
 
@@ -175,7 +190,7 @@ public class AndroidNotification extends PlatformNotification {
         
         
         public AndroidNotification build() {
-            return new AndroidNotification(alert, title, builderId, style, big_text, inbox, big_pic_path, priority,
+            return new AndroidNotification(alert, title, builderId, style, alert_type, big_text, inbox, big_pic_path, priority,
                     category, extrasBuilder, numberExtrasBuilder, booleanExtrasBuilder, jsonExtrasBuilder);
         }
     }
